4a5d9eb4e1bb5765ccb25346f0691af248f10dd6
Dan Bode <dan@puppetlabs.com>
2013-02-12 14:43:17 -0800

    initial commit

---
+++ manifests/init.pp
@@ -0,0 +1,33 @@
+#
+# installs virtual box
+#
+# Only supported on Ubuntu Precise
+#
+class vagrant {
+
+  apt::source { 'virtualbox':
+    location    => 'http://download.virtualbox.org/virtualbox/debian',
+    key         => '98AB5139',
+    key_source  => 'http://download.virtualbox.org/virtualbox/debian/oracle_vbox.asc',
+    repos       => 'contrib',
+    release     => 'precise',
+    pin         => 200,
+    include_src => false,
+  }
+
+  package { 'virtualbox-4.2':
+    ensure  => present,
+    require => Apt::Source['virtualbox'],
+  }
+
+  package { 'rubygems':
+    ensure => present,
+  }
+
+  package { 'vagrant':
+    provider => gem,
+    ensure   => present,
+    require  => Package['virtualbox-4.2', 'rubygems']
+  }
+
+}
